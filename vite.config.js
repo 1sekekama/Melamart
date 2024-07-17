968384249b858ba358b1a3dd7d7e3a991f848d30
Mohamed Deco <110473294+1sekekama@users.noreply.github.com>
2024-07-17 13:32:34 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,12 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rollupOptions: {
-    external: ['react', 'react-dom'],
-    output: {
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDom'
-      }
+
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
     }
   }
-  // build: {
-  //   rollupOptions: {
-  //     external: ['react-router-dom']
-  //   }
-  // }
 })
 
 
